ae1dea746a314b37e743026b52ddfd4251a84d3e
jumper385 <henrychen385@gmail.com>
2020-01-09 14:19:33 +0800

    added the env port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'client/build')))
