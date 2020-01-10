0b1d9890e322b532436852f032c58f1416faf6dc
jumper385 <henrychen385@gmail.com>
2020-01-10 11:18:58 +0800

    ummm....

---
+++ client/build/precache-manifest.0134d3020bd58e1b27390bb9924138e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31a88dc3214c9dc838c7b7807ecccd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7241607582fe01ea6155",
+    "url": "/static/js/2.356901b6.chunk.js"
+  },
+  {
+    "revision": "2ba2081b6c7df8ecdcca12487b4ce022",
+    "url": "/static/js/2.356901b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3cc7ff8109f1ab14f1c",
+    "url": "/static/js/main.53077f4e.chunk.js"
+  },
+  {
+    "revision": "a81123414a52a19941ba",
+    "url": "/static/js/runtime-main.f092ebe6.js"
+  }
+]);
