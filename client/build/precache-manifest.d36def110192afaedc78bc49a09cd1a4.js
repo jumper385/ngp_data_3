5f0469f840b9b4eae065dbe3fc4e684e8c5f6892
jumper385 <henrychen385@gmail.com>
2020-01-20 12:40:56 +0800

    bug fix!?!?!?!?

---
+++ client/build/precache-manifest.d36def110192afaedc78bc49a09cd1a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5187427976d260b299bc036ee299e1fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d937e50cf83a89e641e1",
+    "url": "/static/js/2.bf1964e7.chunk.js"
+  },
+  {
+    "revision": "2ba2081b6c7df8ecdcca12487b4ce022",
+    "url": "/static/js/2.bf1964e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb7737605eb51f178092",
+    "url": "/static/js/main.f392855f.chunk.js"
+  },
+  {
+    "revision": "a81123414a52a19941ba",
+    "url": "/static/js/runtime-main.f092ebe6.js"
+  }
+]);
