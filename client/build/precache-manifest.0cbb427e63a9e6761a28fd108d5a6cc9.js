51c0301a5bf9d9513740bf135a74037ccba36051
jumper385 <henrychen385@gmail.com>
2020-01-28 13:10:36 +0800

    created a new build

---
+++ client/build/precache-manifest.0cbb427e63a9e6761a28fd108d5a6cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70c5765c4350f8647da9cb684610d25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fff14874690dc3bdff0",
+    "url": "/static/js/2.0794bb4d.chunk.js"
+  },
+  {
+    "revision": "fc78e53d1644f591974b7307edc4cd32",
+    "url": "/static/js/2.0794bb4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4658376255deab101732",
+    "url": "/static/js/main.5b422ebf.chunk.js"
+  },
+  {
+    "revision": "e6c9ee952eae46849c5b",
+    "url": "/static/js/runtime-main.f909abda.js"
+  },
+  {
+    "revision": "b528d2d438d49bd12512ea27266f9e46",
+    "url": "/static/media/instructions.b528d2d4.md"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a2214da800cbf02a24a8b3b1e726650.js"
+  "/precache-manifest.0cbb427e63a9e6761a28fd108d5a6cc9.js"
 );
 
 self.addEventListener('message', (event) => {
